878959db62312e78c142a25398cc701d56b10735
Avi Vahl <avi.vahl@wix.com>
2018-10-09 11:35:15 +0300

    Error messages: avoid too much uppercasing

--- packages/memory/src/error-codes.ts
+++ packages/memory/src/error-codes.ts
@@ -1,13 +1,13 @@
 export enum FsErrorCodes {
-    NO_FILE = 'ENOENT: No such file',
-    NO_DIRECTORY = 'ENOENT: No such directory',
-    NO_FILE_OR_DIRECTORY = 'ENOENT: No such file or directory',
+    NO_FILE = 'ENOENT: no such file',
+    NO_DIRECTORY = 'ENOENT: no such directory',
+    NO_FILE_OR_DIRECTORY = 'ENOENT: no such file or directory',
 
-    PATH_IS_FILE = 'ENOTDIR: Path points to a file',
-    PATH_IS_DIRECTORY = 'EISDIR: Path points to a directory',
+    PATH_IS_FILE = 'ENOTDIR: path points to a file',
+    PATH_IS_DIRECTORY = 'EISDIR: path points to a directory',
 
-    CONTAINING_NOT_EXISTS = 'ENOENT: Containing directory does not exist',
-    DIRECTORY_NOT_EMPTY = 'ENOTEMPTY: Directory is not empty',
+    CONTAINING_NOT_EXISTS = 'ENOENT: containing directory does not exist',
+    DIRECTORY_NOT_EMPTY = 'ENOTEMPTY: directory is not empty',
 
-    PATH_ALREADY_EXISTS = 'EEXIST: Path already exists'
+    PATH_ALREADY_EXISTS = 'EEXIST: path already exists'
 }
