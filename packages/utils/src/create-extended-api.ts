080152e82289490e69a3f8e48866c9305929525f
Avi Vahl <avi.vahl@wix.com>
2018-10-18 03:14:06 +0300

    utils: implement async/sync ensureDirectory

--- packages/utils/src/create-extended-api.ts
+++ packages/utils/src/create-extended-api.ts
@@ -1,49 +1,87 @@
 import { IBaseFileSystemAsync, IBaseFileSystemSync, IFileSystemAsync, IFileSystemSync } from '@file-services/types'
 
 export function createSyncFileSystem(baseFs: IBaseFileSystemSync): IFileSystemSync {
-    function fileExistsSync(path: string, statFn = baseFs.statSync): boolean {
+    const { statSync, path, mkdirSync } = baseFs
+
+    function fileExistsSync(filePath: string, statFn = statSync): boolean {
         try {
-            return statFn(path).isFile()
+            return statFn(filePath).isFile()
         } catch {
             return false
         }
     }
 
-    function directoryExistsSync(path: string, statFn = baseFs.statSync): boolean {
+    function directoryExistsSync(directoryPath: string, statFn = statSync): boolean {
         try {
-            return statFn(path).isDirectory()
+            return statFn(directoryPath).isDirectory()
         } catch {
             return false
+        }
+    }
+
+    function ensureDirectorySync(directoryPath: string): void {
+        if (directoryExistsSync(directoryPath)) {
+            return
+        }
+        try {
+            mkdirSync(directoryPath)
+        } catch (e) {
+            const parentPath = path.dirname(directoryPath)
+            if (parentPath === directoryPath) {
+                throw e
+            }
+            ensureDirectorySync(parentPath)
+            mkdirSync(directoryPath)
         }
     }
 
     return {
         ...baseFs,
         fileExistsSync,
-        directoryExistsSync
+        directoryExistsSync,
+        ensureDirectorySync
     }
 }
 
 export function createAsyncFileSystem(baseFs: IBaseFileSystemAsync): IFileSystemAsync {
-    async function fileExists(path: string, statFn = baseFs.stat): Promise<boolean> {
+    const { stat, path, mkdir } = baseFs
+
+    async function fileExists(filePath: string, statFn = stat): Promise<boolean> {
         try {
-            return (await statFn(path)).isFile()
+            return (await statFn(filePath)).isFile()
         } catch {
             return false
         }
     }
 
-    async function directoryExists(path: string, statFn = baseFs.stat): Promise<boolean> {
+    async function directoryExists(directoryPath: string, statFn = stat): Promise<boolean> {
         try {
-            return (await statFn(path)).isDirectory()
+            return (await statFn(directoryPath)).isDirectory()
         } catch {
             return false
+        }
+    }
+
+    async function ensureDirectory(directoryPath: string): Promise<void> {
+        if (await directoryExists(directoryPath)) {
+            return
+        }
+        try {
+            await mkdir(directoryPath)
+        } catch (e) {
+            const parentPath = path.dirname(directoryPath)
+            if (parentPath === directoryPath) {
+                throw e
+            }
+            await ensureDirectory(parentPath)
+            await mkdir(directoryPath)
         }
     }
 
     return {
         ...baseFs,
         fileExists,
-        directoryExists
+        directoryExists,
+        ensureDirectory
     }
 }
