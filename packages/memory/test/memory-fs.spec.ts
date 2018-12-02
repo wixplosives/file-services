93d90624561732d52b3847d4d29e85260f8c622d
Avi Vahl <avi.vahl@wix.com>
2018-12-03 00:05:24 +0200

    Infra: connect sources directly to each other - no longer need to build before running tests - no need to keep cjs and esm versions up to date to see cross-package changes - removed bundling tests. - using @ts-tools/build to build

--- packages/memory/test/memory-fs.spec.ts
+++ packages/memory/test/memory-fs.spec.ts
@@ -1,32 +1,7 @@
-import { join } from 'path'
-import { expect } from 'chai'
-import webpack from 'webpack'
 import { syncBaseFsContract, asyncBaseFsContract, syncFsContract, asyncFsContract } from '@file-services/test-kit'
 import { createMemoryFs } from '../src'
 
 describe('In-memory File System Implementation', () => {
-
-    it('can be bundled using webpack', async () => {
-        const webpackCompiler = webpack({
-            mode: 'development',
-            devtool: false,
-            entry: join(__dirname, '..'), // use root so package.json is picked
-        })
-
-        // don't output to disk
-        webpackCompiler.outputFileSystem = noopOutputFileSystem
-
-        const webpackStats = await new Promise<webpack.Stats>((res, rej) =>
-            webpackCompiler.run((e, s) => e ? rej(e) : res(s))
-        )
-
-        const statsOutputText = webpackStats.toString()
-
-        expect(webpackStats.hasErrors(), statsOutputText).to.equal(false)
-        expect(webpackStats.hasWarnings(), statsOutputText).to.equal(false)
-
-    }).timeout(10000) // bundling can take time (especially on CI), so increase timeout for specific test
-
     const testProvider = async () => {
         const fs = createMemoryFs()
 
@@ -42,12 +17,3 @@ describe('In-memory File System Implementation', () => {
     syncFsContract(testProvider)
     asyncFsContract(testProvider)
 })
-
-const noopOutputFileSystem: webpack.OutputFileSystem = {
-    join,
-    mkdir(_path, callback) { callback(null) },
-    mkdirp(_path, callback) { callback(null) },
-    rmdir(_path, callback) { callback(null) },
-    unlink(_path, callback) { callback(null) },
-    writeFile(_path, _data, callback) { callback(null) }
-}
