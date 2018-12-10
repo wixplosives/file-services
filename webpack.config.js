b538131b6dbcb7a9d7d25ea3ea4e640f755f067a
Avi Vahl <avi.vahl@wix.com>
2018-12-10 12:23:52 +0200

    Add mocha-pup and test @file-services/memory with it also removed now-unused @types/webpack moved --colors flag from root to mocha scripts, as `npm-run-all` doesn't forward parameters

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // root of the monorepo, so that paths in output will be clickable
+    context: __dirname,
+
+    // works great. with the default 'eval', imports are not mapped.
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.mjs', '.js', '.json']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: '@ts-tools/webpack-loader',
+            },
+        ],
+        noParse: [
+            require.resolve('typescript/lib/typescript.js')
+        ]
+    }
+}
