ea5d33566977b7efac34b8937e889c81d218e3b4
Avi Vahl <avi.vahl@wix.com>
2019-03-18 18:57:30 +0200

    webpack: integration test with overlayFs

--- packages/webpack/test/webpack-fs.spec.ts
+++ packages/webpack/test/webpack-fs.spec.ts
@@ -1,7 +1,9 @@
 import { expect } from 'chai';
 import webpack from 'webpack';
+import { nodeFs } from '@file-services/node';
 import { createMemoryFs } from '@file-services/memory';
 import { createWebpackFs } from '@file-services/webpack';
+import { createOverlayFs } from '@file-services/overlay';
 
 describe('createWebpackFs', () => {
     it('allows bundling from and to memory file system', async () => {
@@ -38,5 +40,40 @@ describe('createWebpackFs', () => {
 
         expect(webpackStats.hasWarnings() || webpackStats.hasErrors(), webpackStats.toString()).to.equal(false);
         expect(memFs.fileExistsSync(path.resolve('dist', 'main.js')), 'bundle exists').to.equal(true);
+    });
+
+    it('allows bundling with memory fs over node fs', async () => {
+        const memFs = createMemoryFs({
+            fixture: {
+                'index.js': `import value from './some-file'
+                             console.log(a)`
+            }
+        });
+
+        const webpackFs = createWebpackFs(createOverlayFs(nodeFs, memFs, __dirname));
+        const compiler = webpack({
+            entry: nodeFs.path.join(__dirname, 'fixture'),
+            mode: 'development',
+            context: __dirname,
+            output: {
+                path: memFs.path.resolve('dist') // otherwise it defaults to join(process.cwd(), 'dist')
+            }
+        });
+
+        compiler.inputFileSystem = webpackFs;
+        compiler.outputFileSystem = createWebpackFs(memFs);
+
+        const webpackStats = await new Promise<webpack.Stats>((res, rej) => {
+            compiler.run((e, stats) => {
+                if (e) {
+                    rej(e);
+                } else {
+                    res(stats);
+                }
+            });
+        });
+
+        expect(webpackStats.hasWarnings() || webpackStats.hasErrors(), webpackStats.toString()).to.equal(false);
+        expect(memFs.fileExistsSync(memFs.path.resolve('dist', 'main.js')), 'bundle exists').to.equal(true);
     });
 });
