e8eb5078ad83fe6edaa39fe788f614b532a9e446
Tomer Mesika <tomerme@wix.com>
2019-09-19 17:24:22 +0300

    initial implementation

--- packages/cached/src/cached-fs.ts
+++ packages/cached/src/cached-fs.ts
@@ -1,9 +1,39 @@
-import { IFileSystem } from '@file-services/types';
+import { IFileSystem, IFileSystemStats, CallbackFn } from '@file-services/types';
+import { createFileSystem } from '@file-services/utils';
 
 export interface ICachedFileSystem extends IFileSystem {
     invalidate(path: string): void;
 }
 
 export function createCachedFs(fs: IFileSystem): ICachedFileSystem {
-    return { ...fs, invalidate(path) {} };
+    const statsCache = new Map<string, IFileSystemStats>();
+    return {
+        ...createFileSystem({
+            ...fs,
+            statSync(path: string) {
+                const cachedStats = statsCache.get(path);
+                if (cachedStats) {
+                    return cachedStats;
+                }
+                const stats = fs.statSync(path);
+                statsCache.set(path, stats);
+                return stats;
+            },
+            stat(path: string, callback: CallbackFn<IFileSystemStats>) {
+                const cachedStats = statsCache.get(path);
+                if (cachedStats) {
+                    callback(undefined, cachedStats);
+                    return;
+                }
+                fs.stat(path, (error: Error | null | undefined, stats: IFileSystemStats) => {
+                    statsCache.set(path, stats);
+                    callback(error, stats);
+                    return;
+                });
+            }
+        }),
+        invalidate(path: string) {
+            statsCache.delete(path);
+        }
+    };
 }
--- packages/cached/test/cached-fs.spec.ts
+++ packages/cached/test/cached-fs.spec.ts
@@ -3,24 +3,97 @@ import chaiAsPromised from 'chai-as-promised';
 import { asyncBaseFsContract, syncBaseFsContract } from '@file-services/test-kit';
 import { createMemoryFs } from '@file-services/memory';
 import { createCachedFs } from '../src';
+import sinon from 'sinon';
 
 chai.use(chaiAsPromised);
 
 describe('createCachedFs', () => {
     const SAMPLE_CONTENT = 'content';
 
-    it('can access a file using an absolute path relative to scoped directory', async () => {
+    it('caches statsSync calls', async () => {
         const filePath = '/file';
         const memFs = createMemoryFs({ [filePath]: SAMPLE_CONTENT });
-        // spy on stat/statSync/promises.stat
+
+        const statSyncSpy = sinon.spy(memFs, 'statSync');
+
         const fs = createCachedFs(memFs);
 
         const stats = fs.statSync(filePath);
         const stats2 = fs.statSync(filePath);
 
         expect(stats).to.equal(stats2);
-        expect(memFs.statSync).to.have.been.calledOnce();
+        expect(statSyncSpy.callCount).to.equal(1);
     });
+
+    it('caches statsSync calls with invalidation', async () => {
+        const filePath = '/file';
+        const memFs = createMemoryFs({ [filePath]: SAMPLE_CONTENT });
+
+        const statSyncSpy = sinon.spy(memFs, 'statSync');
+
+        const fs = createCachedFs(memFs);
+
+        const stats = fs.statSync(filePath);
+        fs.invalidate(filePath);
+        const stats2 = fs.statSync(filePath);
+
+        expect(stats).to.not.equal(stats2);
+        expect(statSyncSpy.callCount).to.equal(2);
+    });
+
+    it('caches statsSync calls - through fileExists', async () => {
+        const filePath = '/file';
+        const memFs = createMemoryFs({ [filePath]: SAMPLE_CONTENT });
+
+        const statSyncSpy = sinon.spy(memFs, 'statSync');
+
+        const fs = createCachedFs(memFs);
+
+        const exists = fs.fileExistsSync(filePath);
+        const exists2 = fs.fileExistsSync(filePath);
+
+        expect(exists).to.equal(exists2);
+        expect(statSyncSpy.callCount).to.equal(1);
+    });
+
+    it('caches stats (async) calls', async () => {
+        const filePath = '/file';
+        const memFs = createMemoryFs({ [filePath]: SAMPLE_CONTENT });
+
+        const statSpy = sinon.spy(memFs, 'stat');
+
+        const fs = createCachedFs(memFs);
+
+        expect(statSpy.callCount).to.equal(0);
+
+        fs.stat(filePath, (_error, stats) => {
+            fs.stat(filePath, (_error2, stats2) => {
+                expect(stats).to.equal(stats2);
+                expect(statSpy.callCount).to.equal(1);
+            });
+        });
+    });
+
+    it('caches stats (async) calls with invalidation', async () => {
+        const filePath = '/file';
+        const memFs = createMemoryFs({ [filePath]: SAMPLE_CONTENT });
+
+        const statSpy = sinon.spy(memFs, 'stat');
+
+        const fs = createCachedFs(memFs);
+
+        expect(statSpy.callCount).to.equal(0);
+
+        fs.stat(filePath, (_error, stats) => {
+            fs.invalidate(filePath);
+            fs.stat(filePath, (_error2, stats2) => {
+                expect(stats).to.not.equal(stats2);
+                expect(statSpy.callCount).to.equal(2);
+            });
+        });
+    });
+
+    // with mixing relatives
 
     const testProvider = async () => {
         const fs = createCachedFs(createMemoryFs());
