2b718a377bf4c17a2f900232963e346fbe4ecf4c
Avi Vahl <avi.vahl@wix.com>
2019-09-22 17:53:08 +0300

    cached: auto invalidate for writing actions

--- packages/cached/src/cached-fs.ts
+++ packages/cached/src/cached-fs.ts
@@ -1,4 +1,4 @@
-import { IFileSystem, IFileSystemStats, CallbackFn } from '@file-services/types';
+import { IFileSystem, IFileSystemStats, CallbackFnVoid } from '@file-services/types';
 import { createFileSystem } from '@file-services/utils';
 
 const identity = (val: string) => val;
@@ -29,15 +29,80 @@ export interface IFailureCacheResult {
 export function createCachedFs(fs: IFileSystem): ICachedFileSystem {
     const getCanonicalPath = fs.caseSensitive ? identity : toLowerCase;
     const statsCache = new Map<string, ISuccessCacheResult | IFailureCacheResult>();
-
-    const createCacheKey = (path: string) => getCanonicalPath(fs.resolve(path));
+    const { promises } = fs;
+    const invalidateAbsolute = (absolutePath: string) => statsCache.delete(getCanonicalPath(absolutePath));
 
     return {
         ...createFileSystem({
             ...fs,
-            statSync(path: string) {
-                path = createCacheKey(path);
-                const cachedStats = statsCache.get(path);
+            copyFile: function copyFile(sourcePath: string, destinationPath: string, ...args: [CallbackFnVoid]) {
+                destinationPath = fs.resolve(destinationPath);
+                invalidateAbsolute(destinationPath);
+                return fs.copyFile(sourcePath, destinationPath, ...args);
+            } as IFileSystem['copyFile'],
+            copyFileSync(sourcePath, destinationPath, ...args) {
+                destinationPath = fs.resolve(destinationPath);
+                invalidateAbsolute(destinationPath);
+                return fs.copyFileSync(sourcePath, destinationPath, ...args);
+            },
+            mkdir(directoryPath, callback) {
+                directoryPath = fs.resolve(directoryPath);
+                invalidateAbsolute(directoryPath);
+                return fs.mkdir(directoryPath, callback);
+            },
+            mkdirSync(directoryPath) {
+                directoryPath = fs.resolve(directoryPath);
+                invalidateAbsolute(directoryPath);
+                return fs.mkdirSync(directoryPath);
+            },
+            rename(sourcePath, destinationPath, callback) {
+                sourcePath = fs.resolve(sourcePath);
+                destinationPath = fs.resolve(destinationPath);
+                invalidateAbsolute(sourcePath);
+                invalidateAbsolute(destinationPath);
+                return fs.rename(sourcePath, destinationPath, callback);
+            },
+            renameSync(sourcePath, destinationPath) {
+                sourcePath = fs.resolve(sourcePath);
+                destinationPath = fs.resolve(destinationPath);
+                invalidateAbsolute(sourcePath);
+                invalidateAbsolute(destinationPath);
+                return fs.renameSync(sourcePath, destinationPath);
+            },
+            rmdir(directoryPath, callback) {
+                directoryPath = fs.resolve(directoryPath);
+                invalidateAbsolute(directoryPath);
+                return fs.rmdir(directoryPath, callback);
+            },
+            rmdirSync(directoryPath) {
+                directoryPath = fs.resolve(directoryPath);
+                invalidateAbsolute(directoryPath);
+                return fs.rmdirSync(directoryPath);
+            },
+            unlink(filePath, callback) {
+                filePath = fs.resolve(filePath);
+                invalidateAbsolute(filePath);
+                return fs.unlink(filePath, callback);
+            },
+            unlinkSync(filePath) {
+                filePath = fs.resolve(filePath);
+                invalidateAbsolute(filePath);
+                return fs.unlinkSync(filePath);
+            },
+            writeFile: function writeFile(filePath: string, ...args: [string, CallbackFnVoid]) {
+                filePath = fs.resolve(filePath);
+                invalidateAbsolute(filePath);
+                return fs.writeFile(filePath, ...args);
+            } as IFileSystem['writeFile'],
+            writeFileSync(filePath, ...args) {
+                filePath = fs.resolve(filePath);
+                invalidateAbsolute(filePath);
+                return fs.writeFileSync(filePath, ...args);
+            },
+            statSync(path) {
+                path = fs.resolve(path);
+                const cacheKey = getCanonicalPath(path);
+                const cachedStats = statsCache.get(cacheKey);
                 if (cachedStats) {
                     if (cachedStats.kind === 'failure') {
                         throw cachedStats.error;
@@ -46,16 +111,17 @@ export function createCachedFs(fs: IFileSystem): ICachedFileSystem {
                 }
                 try {
                     const stats = fs.statSync(path);
-                    statsCache.set(path, { kind: 'success', stats });
+                    statsCache.set(cacheKey, { kind: 'success', stats });
                     return stats;
                 } catch (ex) {
-                    statsCache.set(path, { kind: 'failure', error: ex });
+                    statsCache.set(cacheKey, { kind: 'failure', error: ex });
                     throw ex;
                 }
             },
-            stat(path: string, callback: CallbackFn<IFileSystemStats>) {
-                path = createCacheKey(path);
-                const cachedStats = statsCache.get(path);
+            stat(path, callback) {
+                path = fs.resolve(path);
+                const cacheKey = getCanonicalPath(path);
+                const cachedStats = statsCache.get(cacheKey);
                 if (cachedStats) {
                     if (cachedStats.kind === 'failure') {
                         (callback as (e: Error) => void)(cachedStats.error);
@@ -66,9 +132,9 @@ export function createCachedFs(fs: IFileSystem): ICachedFileSystem {
                 }
                 fs.stat(path, (error, stats) => {
                     if (error) {
-                        statsCache.set(path, { kind: 'failure', error });
+                        statsCache.set(cacheKey, { kind: 'failure', error });
                     } else if (stats) {
-                        statsCache.set(path, { kind: 'success', stats });
+                        statsCache.set(cacheKey, { kind: 'success', stats });
                     }
 
                     callback(error, stats);
@@ -76,17 +142,50 @@ export function createCachedFs(fs: IFileSystem): ICachedFileSystem {
                 });
             }
         }),
-        invalidate(path: string) {
-            statsCache.delete(createCacheKey(path));
+        invalidate(path) {
+            return invalidateAbsolute(fs.resolve(path));
         },
         invalidateAll() {
             statsCache.clear();
         },
         promises: {
-            ...fs.promises,
+            ...promises,
+            copyFile(sourcePath, destinationPath, ...args) {
+                destinationPath = fs.resolve(destinationPath);
+                invalidateAbsolute(destinationPath);
+                return promises.copyFile(sourcePath, destinationPath, ...args);
+            },
+            mkdir(directoryPath) {
+                directoryPath = fs.resolve(directoryPath);
+                invalidateAbsolute(directoryPath);
+                return promises.mkdir(directoryPath);
+            },
+            rename(sourcePath, destinationPath) {
+                sourcePath = fs.resolve(sourcePath);
+                destinationPath = fs.resolve(destinationPath);
+                invalidateAbsolute(sourcePath);
+                invalidateAbsolute(destinationPath);
+                return promises.rename(sourcePath, destinationPath);
+            },
+            rmdir(directoryPath) {
+                directoryPath = fs.resolve(directoryPath);
+                invalidateAbsolute(directoryPath);
+                return promises.rmdir(directoryPath);
+            },
+            unlink(filePath) {
+                filePath = fs.resolve(filePath);
+                invalidateAbsolute(filePath);
+                return promises.unlink(filePath);
+            },
+            writeFile(filePath, ...args) {
+                filePath = fs.resolve(filePath);
+                invalidateAbsolute(filePath);
+                return promises.writeFile(filePath, ...args);
+            },
             async stat(path: string) {
-                path = createCacheKey(path);
-                const cachedStats = statsCache.get(path);
+                path = fs.resolve(path);
+                const cacheKey = getCanonicalPath(path);
+                const cachedStats = statsCache.get(cacheKey);
                 if (cachedStats) {
                     if (cachedStats.kind === 'failure') {
                         throw cachedStats.error;
@@ -94,13 +193,11 @@ export function createCachedFs(fs: IFileSystem): ICachedFileSystem {
                     return cachedStats.stats;
                 }
                 try {
-                    const stats = await new Promise((res: (value: IFileSystemStats) => void, rej) =>
-                        fs.stat(path, (error, value) => (error ? rej(error) : res(value)))
-                    );
-                    statsCache.set(path, { kind: 'success', stats });
+                    const stats = await promises.stat(path);
+                    statsCache.set(cacheKey, { kind: 'success', stats });
                     return stats;
                 } catch (ex) {
-                    statsCache.set(path, { kind: 'failure', error: ex });
+                    statsCache.set(cacheKey, { kind: 'failure', error: ex });
                     throw ex;
                 }
             }
