28ed12934289e452304eafbaf33d2a370c3f7777
Avi Vahl <avi.vahl@wix.com>
2019-09-30 16:21:40 +0300

    Deps: upgrade to latest stable - added `tsconfig-paths-webpack-plugin` for "paths" field support. - regenerated yarn.lock.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const { join } = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
     // root of the monorepo, so that paths in output will be clickable
     context: __dirname,
@@ -6,15 +9,16 @@ module.exports = {
     devtool: 'source-map',
 
     resolve: {
-        extensions: ['.ts', '.tsx', '.mjs', '.js', '.json']
+        extensions: ['.ts', '.tsx', '.mjs', '.js', '.json'],
+        plugins: [new TsconfigPathsPlugin({ configFile: join(__dirname, 'tsconfig.json') })]
     },
 
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: '@ts-tools/webpack-loader',
-            },
+                loader: '@ts-tools/webpack-loader'
+            }
         ]
     }
-}
+};
