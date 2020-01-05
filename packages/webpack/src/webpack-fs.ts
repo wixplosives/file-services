47c93d49008a611f79631968c9e96cc0c20ed7d1
Avi Vahl <avi.vahl@wix.com>
2020-01-06 00:38:23 +0200

    webpack: implement readJson/readJsonSync closes #37 CachedFileSystem implements them under these names, even though it's not declared in webpack's types.

--- packages/webpack/src/webpack-fs.ts
+++ packages/webpack/src/webpack-fs.ts
@@ -1,16 +1,30 @@
 import webpack from 'webpack';
-import { IFileSystem } from '@file-services/types';
+import { IFileSystem, CallbackFn } from '@file-services/types';
 
-export function createWebpackFs(fs: IFileSystem): webpack.InputFileSystem & webpack.OutputFileSystem {
+export interface IWebpackFileSystem extends webpack.InputFileSystem, webpack.OutputFileSystem {
+    readJson(filePath: string, callback: CallbackFn<unknown>): void;
+    readJsonSync(filePath: string): unknown;
+}
+
+export function createWebpackFs(fs: IFileSystem): IWebpackFileSystem {
     const {
         watchService,
-        promises: { ensureDirectory }
+        readJsonFileSync,
+        promises: { ensureDirectory, readJsonFile }
     } = fs;
 
     return {
         ...fs,
+        readJsonSync: readJsonFileSync,
+        readJson(filePath, callback) {
+            readJsonFile(filePath)
+                .then(value => callback(undefined, value))
+                .catch(e => callback(e, undefined));
+        },
         mkdirp(directoryPath, callback) {
-            ensureDirectory(directoryPath).then(() => callback(undefined), callback);
+            ensureDirectory(directoryPath)
+                .then(() => callback(undefined))
+                .catch(callback);
         },
         async purge() {
             watchService.clearGlobalListeners();
