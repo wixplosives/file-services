d24eca386524b1793f736c7e1c843ee9d54260f8
Avi Vahl <avi.vahl@wix.com>
2023-02-07 15:20:42 +0200

    chore(deps-dev): upgrade to latest stable and regenerate lock file

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,3 +1,6 @@
 export default {
-    pinnedPackages: [{ name: 'rimraf', reason: 'v4 dropped glob support' }],
+  pinnedPackages: [
+    { name: 'rimraf', reason: 'v4 dropped glob support' },
+    { name: '@types/rimraf', reason: 'v4 dropped glob support' },
+  ],
 };
