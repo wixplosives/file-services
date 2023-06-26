1f8bf24e9d68a84c6234909c0cdd734996129e4a
Avi Vahl <avi.vahl@wix.com>
2023-06-26 17:03:55 +0300

    feat(path): ensure compat with native esm import still works with interop or raw require calls

--- packages/path/index.js
+++ packages/path/index.js
@@ -1,1 +1,19 @@
-module.exports = require('path');
+"use strict";
+Object.defineProperty(exports, '__esModule', { value: true });
+const path = require('path');
+
+exports.default = exports;
+exports.resolve = path.resolve;
+exports.normalize = path.normalize;
+exports.isAbsolute = path.isAbsolute;
+exports.join = path.join;
+exports.relative = path.relative;
+exports.dirname = path.dirname;
+exports.basename = path.basename;
+exports.extname = path.extname;
+exports.format = path.format;
+exports.parse = path.parse;
+exports.sep = path.sep;
+exports.delimiter = path.delimiter;
+exports.win32 = path.win32;
+exports.posix = path.posix;
