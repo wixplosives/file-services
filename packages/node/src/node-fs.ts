1f8cf722b0409b8bd57c5a64d68962109f6d1052
Avi Vahl <avi@vahl.co.il>
2023-09-27 01:42:31 +0300

    fix(node): use cwd for case-sensitivity check - to fix mjs target not working due to accessing __filename - import from "node:<lib>" for clarity

--- packages/node/src/node-fs.ts
+++ packages/node/src/node-fs.ts
@@ -1,13 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import { chdir, cwd } from 'process';
-import { promisify } from 'util';
+import fs from 'node:fs';
+import path from 'node:path';
+import { chdir, cwd } from 'node:process';
+import { promisify } from 'node:util';
 
-import { createFileSystem } from '@file-services/utils';
 import type { IBaseFileSystem, IFileSystem, IFileSystemPath } from '@file-services/types';
-import { NodeWatchService, INodeWatchServiceOptions } from './watch-service';
+import { createFileSystem } from '@file-services/utils';
+import { INodeWatchServiceOptions, NodeWatchService } from './watch-service';
 
-const caseSensitive = !fs.existsSync(__filename.toUpperCase());
+const caseSensitive = !fs.existsSync(cwd().toUpperCase());
 const fsPromisesExists = promisify(fs.exists);
 
 export interface ICreateNodeFsOptions {
