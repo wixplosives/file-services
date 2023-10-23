1ce7f87d54fa48e526d8e6290cc4800313ba0a34
Avi Vahl <avi.vahl@wix.com>
2023-10-24 01:47:44 +0300

    fix(node): use argv[0] for case detection better than cwd, which can be just C:\ or not exist see: https://github.com/wixplosives/file-services/pull/991#discussion_r1359940230

--- packages/node/src/node-fs.ts
+++ packages/node/src/node-fs.ts
@@ -1,13 +1,13 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { chdir, cwd } from 'node:process';
+import { chdir, cwd, argv } from 'node:process';
 import { promisify } from 'node:util';
 
 import type { IBaseFileSystem, IFileSystem, IFileSystemPath } from '@file-services/types';
 import { createFileSystem } from '@file-services/utils';
 import { INodeWatchServiceOptions, NodeWatchService } from './watch-service';
 
-const caseSensitive = !fs.existsSync(cwd().toUpperCase());
+const caseSensitive = !fs.existsSync(argv[0]!.toUpperCase());
 const fsPromisesExists = promisify(fs.exists);
 
 export interface ICreateNodeFsOptions {
