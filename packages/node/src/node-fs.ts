a3900adf7091bc4e04d8cefcbc2724adcbad7ae8
Avi Vahl <avi.vahl@wix.com>
2024-03-24 18:24:55 +0200

    refactor: remove now-unneeded cast

--- packages/node/src/node-fs.ts
+++ packages/node/src/node-fs.ts
@@ -1,9 +1,9 @@
 import fs from "node:fs";
 import path from "node:path";
-import { chdir, cwd, argv } from "node:process";
+import { argv, chdir, cwd } from "node:process";
 import { promisify } from "node:util";
 
-import type { IBaseFileSystem, IFileSystem, IFileSystemPath } from "@file-services/types";
+import type { IBaseFileSystem, IFileSystem } from "@file-services/types";
 import { createFileSystem } from "@file-services/utils";
 import { INodeWatchServiceOptions, NodeWatchService } from "./watch-service";
 
@@ -20,7 +20,7 @@ export function createNodeFs(options?: ICreateNodeFsOptions): IFileSystem {
 
 export function createBaseNodeFs(options?: ICreateNodeFsOptions): IBaseFileSystem {
   return {
-    ...(path as IFileSystemPath),
+    ...path,
     chdir,
     cwd,
     watchService: new NodeWatchService(options && options.watchOptions),
