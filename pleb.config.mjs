7bcd5ea1de36ed0e4868f0c91cc787f8a74bb858
Avi Vahl <avi.vahl@wix.com>
2024-06-05 14:26:06 +0300

    infra: reformat sources using prettier

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,6 +1,6 @@
 export default {
   pinnedPackages: [
-    { name: 'chai', reason: 'v5 is pure esm' },
-    { name: 'eslint', reason: 'plugins are not yet compatible with v9' },
+    { name: "chai", reason: "v5 is pure esm" },
+    { name: "eslint", reason: "plugins are not yet compatible with v9" },
   ],
 };
