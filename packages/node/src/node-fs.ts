d6d199c98271bd01cfefd5a9d43ca7e6caa727fd
Avi Vahl <avi@vahl.co.il>
2024-06-09 03:30:31 +0300

    fix(node): use own recursive watch under Linux workaround unhandled exceptions during runtime and/or close() in Node's own recursive watcher. reported upstream.
    now using own implementation of recursive FSWatcher which implements the subset api file-services exposes.

--- packages/node/src/node-fs.ts
+++ packages/node/src/node-fs.ts
@@ -3,9 +3,10 @@ import path from "node:path";
 import { argv, chdir, cwd } from "node:process";
 import { promisify } from "node:util";
 
-import type { IBaseFileSystem, IFileSystem } from "@file-services/types";
+import type { IBaseFileSystem, IFileSystem, WatchOptions } from "@file-services/types";
 import { createFileSystem } from "@file-services/utils";
 import { INodeWatchServiceOptions, NodeWatchService } from "./watch-service";
+import { RecursiveFSWatcher } from "./recursive-fs-watcher";
 
 const caseSensitive = !fs.existsSync(argv[0]!.toUpperCase());
 const fsPromisesExists = promisify(fs.exists);
@@ -19,6 +20,8 @@ export function createNodeFs(options?: ICreateNodeFsOptions): IFileSystem {
 }
 
 export function createBaseNodeFs(options?: ICreateNodeFsOptions): IBaseFileSystem {
+  const originalWatch = fs.watch;
+  const watch = process.platform === "linux" ? wrapWithOwnRecursiveImpl(originalWatch) : originalWatch;
   return {
     ...path,
     chdir,
@@ -26,9 +29,19 @@ export function createBaseNodeFs(options?: ICreateNodeFsOptions): IBaseFileSyste
     watchService: new NodeWatchService(options && options.watchOptions),
     caseSensitive,
     ...fs,
+    watch,
     promises: {
       ...fs.promises,
       exists: fsPromisesExists,
     },
+  };
+}
+
+function wrapWithOwnRecursiveImpl(originalWatch: typeof fs.watch) {
+  return (targetPath: string, options?: WatchOptions) => {
+    if (options?.recursive) {
+      return new RecursiveFSWatcher(targetPath, options);
+    }
+    return originalWatch(targetPath, options);
   };
 }
