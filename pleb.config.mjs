5d637b8b7768c24f5ea9c49ca89797b0e2713258
Avi Vahl <avi@vahl.co.il>
2024-07-26 16:43:48 +0300

    infra: ignore sinon-chai@4

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -2,6 +2,7 @@ export default {
   pinnedPackages: [
     { name: "chai", reason: "v5 is pure esm" },
     { name: "chai-as-promised", reason: "v8 is pure esm" },
+    { name: 'sinon-chai', reason: 'v4 requires chai@5' },
     { name: "eslint", reason: "plugins are not yet compatible with v9" },
     { name: "rimraf", reason: "v6 drops node 18 support" },
   ],
