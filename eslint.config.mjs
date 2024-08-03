7032cb3c415e4b022758bc7333e8fc7aa5e33b9a
Avi Vahl <avi@vahl.co.il>
2024-08-03 20:52:37 +0300

    infra: re-add eslint-plugin-no-only-tests use @eslint/compat to adapt it to eslint@9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,25 @@
+import { fixupPluginRules } from "@eslint/compat";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import configPrettier from "eslint-config-prettier";
+import pluginNoOnlyTests from "eslint-plugin-no-only-tests";
+import pluginTypescript from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { ignores: ["**/dist/", "**/*.{js,mjs,cjs}"] },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+
+  ...pluginTypescript.configs.recommendedTypeChecked,
   { languageOptions: { parserOptions: { projectService: true } } },
+  { plugins: { "no-only-tests": fixupPluginRules(pluginNoOnlyTests) } },
+  configPrettier,
+
   {
     rules: {
       "no-console": "error",
+      "no-only-tests/no-only-tests": "error",
       "@typescript-eslint/require-await": "off",
       "@typescript-eslint/unbound-method": "off",
     },
   },
-  configPrettier,
 ];
