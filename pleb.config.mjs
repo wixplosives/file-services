7583951d2610fe9803bf69b7b106ea1e888893b6
Avi Vahl <avi@vahl.co.il>
2024-09-25 12:18:32 +0300

    infra: shorter pinned versions reasons

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,12 +1,13 @@
 export default {
   pinnedPackages: [
-    { name: "chai", reason: "v5 is pure esm" },
-    { name: "@types/chai", reason: "v8 is pure esm" },
-    { name: "chai-as-promised", reason: "v8 is pure esm" },
-    { name: "@types/chai-as-promised", reason: "v8 is pure esm" },
-    { name: "sinon-chai", reason: "v4 requires chai@5" },
-    { name: "@types/sinon-chai", reason: "v8 is pure esm" },
-    { name: "rimraf", reason: "v6 drops node 18 support" },
-    { name: "mocha-web", reason: "v2 drops node 18 support" },
+    { name: "chai", reason: "esm-only" },
+    { name: "@types/chai", reason: "esm-only" },
+    { name: "chai-as-promised", reason: "esm-only" },
+    { name: "@types/chai-as-promised", reason: "esm-only" },
+    { name: "sinon-chai", reason: "esm-only" },
+    { name: "@types/sinon-chai", reason: "esm-only" },
+
+    { name: "rimraf", reason: "drops node 18 support" },
+    { name: "mocha-web", reason: "drops node 18 support" },
   ],
 };
