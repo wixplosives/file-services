380fbd79e228d6bb385f89a17f13aec3df8736c0
Avi Vahl <avi.vahl@wix.com>
2025-04-07 14:05:09 +0300

    feat!: remove linux recursive watcher shim (#1372)
    unneeded as of newer Node versions

--- packages/node/src/node-fs.ts
+++ packages/node/src/node-fs.ts
@@ -3,9 +3,8 @@ import path from "node:path";
 import { argv, chdir, cwd } from "node:process";
 import { promisify } from "node:util";
 
-import type { IBaseFileSystem, IFileSystem, WatchOptions } from "@file-services/types";
+import type { IBaseFileSystem, IFileSystem } from "@file-services/types";
 import { createFileSystem } from "@file-services/utils";
-import { RecursiveFSWatcher } from "./recursive-fs-watcher";
 
 const caseSensitive = !fs.existsSync(argv[0]!.toUpperCase());
 const fsPromisesExists = promisify(fs.exists);
@@ -15,27 +14,15 @@ export function createNodeFs(): IFileSystem {
 }
 
 export function createBaseNodeFs(): IBaseFileSystem {
-  const originalWatch = fs.watch;
-  const watch = process.platform === "linux" ? wrapWithOwnRecursiveImpl(originalWatch) : originalWatch;
   return {
     ...path,
     chdir,
     cwd,
     caseSensitive,
     ...fs,
-    watch,
     promises: {
       ...fs.promises,
       exists: fsPromisesExists,
     },
-  };
-}
-
-function wrapWithOwnRecursiveImpl(originalWatch: typeof fs.watch) {
-  return (targetPath: string, options?: WatchOptions) => {
-    if (options?.recursive) {
-      return new RecursiveFSWatcher(targetPath, options);
-    }
-    return originalWatch(targetPath, options);
   };
 }
