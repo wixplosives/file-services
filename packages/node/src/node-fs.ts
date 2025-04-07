621ca46a530937d4f0b21866285d0a5795e7342b
Avi Vahl <avi.vahl@wix.com>
2025-04-07 12:40:50 +0300

    cleanup!: remove deprecated WatchService (#1258)
    implementation has race condition bugs. superseded by the official .watch() api.

--- packages/node/src/node-fs.ts
+++ packages/node/src/node-fs.ts
@@ -5,28 +5,22 @@ import { promisify } from "node:util";
 
 import type { IBaseFileSystem, IFileSystem, WatchOptions } from "@file-services/types";
 import { createFileSystem } from "@file-services/utils";
-import { INodeWatchServiceOptions, NodeWatchService } from "./watch-service";
 import { RecursiveFSWatcher } from "./recursive-fs-watcher";
 
 const caseSensitive = !fs.existsSync(argv[0]!.toUpperCase());
 const fsPromisesExists = promisify(fs.exists);
 
-export interface ICreateNodeFsOptions {
-  watchOptions?: INodeWatchServiceOptions;
+export function createNodeFs(): IFileSystem {
+  return createFileSystem(createBaseNodeFs());
 }
 
-export function createNodeFs(options?: ICreateNodeFsOptions): IFileSystem {
-  return createFileSystem(createBaseNodeFs(options));
-}
-
-export function createBaseNodeFs(options?: ICreateNodeFsOptions): IBaseFileSystem {
+export function createBaseNodeFs(): IBaseFileSystem {
   const originalWatch = fs.watch;
   const watch = process.platform === "linux" ? wrapWithOwnRecursiveImpl(originalWatch) : originalWatch;
   return {
     ...path,
     chdir,
     cwd,
-    watchService: new NodeWatchService(options && options.watchOptions),
     caseSensitive,
     ...fs,
     watch,
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,4 +2,3 @@ export * from "./base-api";
 export * from "./common-fs-types";
 export * from "./extended-api";
 export * from "./path";
-export * from "./watch-api";
